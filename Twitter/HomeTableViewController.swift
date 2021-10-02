b27df4103a6d6b8f324cc785a874747043062a5e
David Nguyen <david.nguyen16@mavs.uta.edu>
2021-10-01 21:28:10 -0500

    twitter part 2 attempted

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,11 +17,21 @@ class HomeTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
-        
+        numberOfTweets = 20
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
-   
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.loadTweets()
     }
     
     @objc func loadTweets() {
@@ -43,6 +53,7 @@ class HomeTableViewController: UITableViewController {
             self.myRefreshControl.endRefreshing()
             
         }, failure: { Error in
+            print(Error.localizedDescription)
             print("Could not retrieve tweets! oh no!")
         })
     }
@@ -66,6 +77,7 @@ class HomeTableViewController: UITableViewController {
             self.myRefreshControl.endRefreshing()
             
         }, failure: { Error in
+            
             print("Could not retrieve tweets! oh no!")
         })
     }
@@ -100,6 +112,10 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+       
         return cell
     }
   
